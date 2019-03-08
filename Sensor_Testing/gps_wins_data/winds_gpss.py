238c7d50096c4083f2060ed7934a146998b350a8
unknown <luke.hartlage@gmail.com>
2019-03-07 17:31:02 -0800

    performance improvement + comments

--- Sensor_Testing/gps_wins_data/winds_gpss.py
+++ Sensor_Testing/gps_wins_data/winds_gpss.py
@@ -1,6 +1,7 @@
 import sys
 from decimal import Decimal
 
+# Loop thorugh all files passed by user
 for i in range(1,len(sys.argv)):
 	f = open(sys.argv[i])
 	print("Opened argv number", i, sys.argv[i])
@@ -9,12 +10,15 @@ for i in range(1,len(sys.argv)):
 	there = {}
 	back = {}
 
+	# get file size and print to user for stats purposes
 	t = 0
 	for line in f:
 		lineDict[t] = line.rstrip('\n').split(",")
 		t+=1
 	print("File size:", len(lineDict), "lines")
 
+	# Add unique (defined by windSpeed) items to "there" array until turnaround point detected
+	# after turnaround point detected, add unique items to "back" array instead
 	midVal = "-1"
 	for x in range(20, len(lineDict)-40):
 		if midVal == "-1":
@@ -58,84 +62,87 @@ for i in range(1,len(sys.argv)):
 					break
 			continue
 
+	avgd = {}
+	found = 0
+	# loop through all items in "there"
+	# for each, find all items in "back" within 0.1 gpsSpeed
+	# if none within 0.1, try within 0.2, and then 0.3
+	# after stuff is found, take average of all things found within tolerance to get average windSpeed for given gpsSpeed
+	for keyT in there:
+		found = 0
+		numFound = 0
+		tracking = 0
+		for keyB in back:
+			if abs(Decimal(keyT) - Decimal(keyB)) < 0.1:
+				found = 1
+				if tracking == 0:
+					tracking = str((Decimal(keyT) + Decimal(keyB))/2)
+				if tracking not in avgd:
+					avgd[tracking] = 0
+				avgd[tracking] += ((Decimal(there[keyT]) + Decimal(back[keyB])) / 2) / Decimal(1.15)
+				numFound += 1
+		if numFound > 0:
+			avgd[tracking] /= numFound
+		if found == 1:
+			continue;
+		numFound = 0
+		tracking = 0
+		for keyB in back:
+			if abs(Decimal(keyT) - Decimal(keyB)) < 0.2:
+				found = 1
+				if tracking == 0:
+					tracking = str((Decimal(keyT) + Decimal(keyB))/2)
+				if tracking not in avgd:
+					avgd[tracking] = 0
+				avgd[tracking] += ((Decimal(there[keyT]) + Decimal(back[keyB])) / 2) / Decimal(1.15)
+				numFound += 1
+		if numFound > 0:
+			avgd[tracking] /= numFound
+		if found == 1:
+			continue;
+		numFound = 0
+		tracking = 0
+		for keyB in back:
+			if abs(Decimal(keyT) - Decimal(keyB)) < 0.3:
+				found = 1
+				if tracking == 0:
+					tracking = str((Decimal(keyT) + Decimal(keyB))/2)
+				if tracking not in avgd:
+					avgd[tracking] = 0
+				avgd[tracking] += ((Decimal(there[keyT]) + Decimal(back[keyB])) / 2) / Decimal(1.15)
+				numFound += 1
+		if numFound > 0:
+			avgd[tracking] /= numFound
+
+	# Scaling range: 0.9-1.3
+	# Complete average error checking with different scale values to find best fit scaling
 	bestAvg = 5
 	bestScale = 0
 	for z in range(0, 40):
 		scaleFactor = Decimal(0.9) + Decimal(z / 100)
-		avgd = {}
-		found = 0
-		for keyT in there:
-			found = 0
-			numFound = 0
-			tracking = 0
-			for keyB in back:
-				if abs(Decimal(keyT) - Decimal(keyB)) < 0.1:
-					found = 1
-					if tracking == 0:
-						tracking = str((Decimal(keyT) + Decimal(keyB))/2)
-					if tracking not in avgd:
-						avgd[tracking] = 0
-					avgd[tracking] += Decimal(scaleFactor*((Decimal(there[keyT]) + Decimal(back[keyB])) / 2) / Decimal(1.15))
-					numFound += 1
-			if numFound > 0:
-				avgd[tracking] /= numFound
-			if found == 1:
-				continue;
-			numFound = 0
-			tracking = 0
-			for keyB in back:
-				if abs(Decimal(keyT) - Decimal(keyB)) < 0.2:
-					found = 1
-					if tracking == 0:
-						tracking = str((Decimal(keyT) + Decimal(keyB))/2)
-					if tracking not in avgd:
-						avgd[tracking] = 0
-					avgd[tracking] += Decimal(scaleFactor*((Decimal(there[keyT]) + Decimal(back[keyB])) / 2) / Decimal(1.15))
-					numFound += 1
-			if numFound > 0:
-				avgd[tracking] /= numFound
-			if found == 1:
-				continue;
-			numFound = 0
-			tracking = 0
-			for keyB in back:
-				if abs(Decimal(keyT) - Decimal(keyB)) < 0.3:
-					found = 1
-					if tracking == 0:
-						tracking = str((Decimal(keyT) + Decimal(keyB))/2)
-					if tracking not in avgd:
-						avgd[tracking] = 0
-					avgd[tracking] += Decimal(scaleFactor*((Decimal(there[keyT]) + Decimal(back[keyB])) / 2) / Decimal(1.15))
-					numFound += 1
-			if numFound > 0:
-				avgd[tracking] /= numFound
-		maxErr = 0
+
+		# for every (gpsSpeed, avgWindSpeed) pair, calculate error
+		# find the average and max of all these errors
+		# if these errors are less than current best for given scaling factor, replace best scaling with this scaling 
+		maxErr = 0	
 		totErr = 0
 		totCt = 0
-
 		for k in avgd:
-			if Decimal(avgd[k]) > 30:
-				#print("OUTLIER: ", avgd[k])
+			if Decimal(scaleFactor)*Decimal(avgd[k]) > 20:
 				continue
-			totErr += abs(Decimal(k) - Decimal(avgd[k]))
+			totErr += abs(Decimal(k) - Decimal(scaleFactor)*Decimal(avgd[k]))
 			totCt += 1
-			if abs(Decimal(k) - Decimal(avgd[k])) > maxErr:
-				maxErr = abs(Decimal(k) - Decimal(avgd[k])) 
-			#print("--GPS:", k," ANEMOM:", avgd[k])
+			if abs(Decimal(k) - Decimal(scaleFactor)*Decimal(avgd[k])) > maxErr:
+				maxErr = abs(Decimal(k) - Decimal(scaleFactor)*Decimal(avgd[k]))
 
 		if totCt != 0:
 			if totErr / totCt < bestAvg:
 				bestAvg = totErr / totCt
 				bestScale = scaleFactor
 				bestMaxErr = maxErr
-			#print("--Error Avg:", totErr / totCt)
-			#print("--Error Max:", maxErr)
 
 	print("--Best ScaleFactor:", bestScale)
 	print("--Avg Err w/ best ScaleFactor:", bestAvg)
 	print("--MaxErr w/ best ScaleFactor:", bestMaxErr)
-
-	#print("--There size", len(there))
-	#print("--Back size", len(back))
 
 	f.close()
